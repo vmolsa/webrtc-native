95ca1886591ea26d3906b5873f41e644d27c3de8
Ville Mölsä <ville.molsa@gmail.com>
2015-04-28 20:39:19 +0300

    Add GetUserMedia

--- src/GetUserMedia.cc
+++ src/GetUserMedia.cc
@@ -37,5 +37,56 @@ void GetUserMedia::Init(v8::Handle<v8::Object> exports) {
 }
 
 void GetUserMedia::OpenMedia(const FunctionCallbackInfo<Value> &args) {
+  Isolate* isolate = args.GetIsolate();
+  HandleScope scope(isolate);
+
+  Local<Object> constraints;  // TODO(): Required? -> Throw Error
+  Local<Function> onsuccess;  // TODO(): Required? -> Throw Error 
+  Local<Function> onerror;    // TODO(): Required? -> Throw Error
+
+  bool echo = true;
+
+  uint32_t min_width = 320;
+  uint32_t max_width = 1280;
+
+  uint32_t min_height = 180;
+  uint32_t max_height = 720;
+
+  double min_fps = 24.0;
+  double max_fps = 60.0;
+
+  double min_aspectRatio = 1.33333333333; //  4/3 
+  double max_aspectRatio = 1.7777777778;  //  16/9
+
+  std::string audioId;
+  std::string videoId;
+  
+  if (args.Length() >= 1 && args[0]->IsObject()) {
+    constraints = Local<Object>::Cast(args[0]);
+
+    if (args.Length() >= 2 && args[1]->IsFunction()) {
+      onsuccess = Local<Function>::Cast(args[1]);
+    }
+
+    if (args.Length() == 3 && args[2]->IsFunction()) {
+      onerror = Local<Function>::Cast(args[2]);
+    }
+  }
+
+  if (!constraints.IsEmpty()) {
+    Local<Value> audio_value = constraints->Get(String::NewFromUtf8(isolate, "audio"));
+    Local<Value> video_value = constraints->Get(String::NewFromUtf8(isolate, "video"));
+
+    if (!audio_value.IsEmpty() && audio_value->IsObject()) {
+      Local<Object> audio_obj = Local<Object>::Cast(audio_value);
+
+    }
+
+    if (!video_value.IsEmpty() && video_value->IsObject()) {
+      Local<Object> video_obj = Local<Object>::Cast(video_value);
+
+    }
+  }
+
   // TODO(): Implement This
 }
