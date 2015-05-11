d158e68a8b23e8e97232bc8ea53ebbbe671a49d9
vmolsa <ville.molsa@gmail.com>
2015-05-11 10:44:14 +0300

    Fix order

--- src/PeerConnectionTest.cc
+++ src/PeerConnectionTest.cc
@@ -29,10 +29,10 @@
 using namespace WebRTC;
 
 PeerConnection::PeerConnection(const char *id) :
-  _id(id),
   OnIceCandidate(0),
   OnNegotiationNeeded(0),
   OnDataChannel(0),
+  _id(0),
   _onoffer(0),
   _onanswer(0),
   _onlocal(0),
