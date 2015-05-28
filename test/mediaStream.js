91d4707f8970dc14eaa10517d467baaf08f96a4e
Ville Mölsä <ville.molsa@gmail.com>
2015-05-29 02:00:04 +0300

    Update

--- test/mediaStream.js
+++ test/mediaStream.js
@@ -9,8 +9,36 @@ var config = {
 };
 
 var constraints = {
-  audio: true,
-  video: true,
+  audio: {
+    optional: [
+      {
+        googEchoCancellation: true,
+        googEchoCancellation2: true,
+        googDAEchoCancellation: true,
+        googAutoGainControl: true,
+        googAutoGainControl2: true,
+        googNoiseSuppression: true,
+        googNoiseSuppression2: true,
+        googHighpassFilter: true,
+        googTypingNoiseDetection: true,
+        googAudioMirroring: true,
+      },
+    ],
+  },
+  video: {
+    optional: [
+      {
+        minAspectRatio: 1.333,
+        maxAspectRatio: 1.778,
+        maxWidth: 1920,
+        minWidth: 320,
+        maxHeight: 1080,
+        minHeight: 180,
+        maxFrameRate: 60,
+        minFrameRate: 30,
+      },
+    ],
+  },
   optional: [
     {
       DtlsSrtpKeyAgreement: true,
