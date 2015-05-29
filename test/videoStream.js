bd32a6200fd1826a63875b8e0b30c3e777f32d8c
vmolsa <ville.molsa@gmail.com>
2015-05-29 03:04:54 +0300

    Add video stream test

---
+++ test/videoStream.js
@@ -0,0 +1,59 @@
+var WEBRTC = require('../');
+
+WEBRTC.setDebug(true);
+
+function onSuccess(stream) {
+  console.log('Got VideoStream');
+}
+
+function onError(error) {
+  throw error;
+}
+
+var constraints = {
+/*
+  audio: {
+    optional: [
+      {
+        googEchoCancellation: true,
+        googEchoCancellation2: true,
+        googDAEchoCancellation: true,
+        googAutoGainControl: true,
+        googAutoGainControl2: true,
+        googNoiseSuppression: true,
+        googNoiseSuppression2: true,
+        googHighpassFilter: true,
+        googTypingNoiseDetection: true,
+        googAudioMirroring: true,
+      },
+    ],
+  },
+*/
+  video: {
+    optional: [
+      {
+        minAspectRatio: 1.333,
+        maxAspectRatio: 1.778,
+        maxWidth: 1920,
+        minWidth: 320,
+        maxHeight: 1080,
+        minHeight: 180,
+        maxFrameRate: 60,
+        minFrameRate: 30,
+      },
+    ],
+  },
+/*
+  optional: [
+    {
+      DtlsSrtpKeyAgreement: true,
+    },
+  ],
+  mandatory: {
+    OfferToReceiveAudio: true,
+    OfferToReceiveVideo: true,
+  },
+*/
+};
+
+WEBRTC.getUserMedia(constraints, onSuccess, onError);
