3a6ca68bf29d73d7db5b9b03ea240e5219d3eb08
Ville Mölsä <ville.molsa@gmail.com>
2015-06-14 01:17:15 +0300

    Update stream test

--- test/p2p_stream.js
+++ test/p2p_stream.js
@@ -1,5 +1,7 @@
 var WEBRTC = require('../');
 
+WEBRTC.setDebug(true);
+
 var config = {
   iceServers: [
     {
@@ -9,9 +11,39 @@ var config = {
 };
 
 var constraints = {
+  audio: {
+    optional: [
+      {
+        googEchoCancellation: true,
+        googEchoCancellation2: true,
+        googDAEchoCancellation: true,
+        googAutoGainControl: true,
+        googAutoGainControl2: true,
+        googNoiseSuppression: true,
+        googNoiseSuppression2: true,
+        googHighpassFilter: true,
+        googTypingNoiseDetection: true,
+        googAudioMirroring: true,
+      },
+    ],
+  },
+  video: {
+    optional: [
+      {
+        minAspectRatio: 1.333,
+        maxAspectRatio: 1.778,
+        maxWidth: 1920,
+        minWidth: 320,
+        maxHeight: 1080,
+        minHeight: 180,
+        maxFrameRate: 60,
+        minFrameRate: 30,
+      },
+    ],
+  },
   optional: [
     {
-      DtlsSrtpKeyAgreement: false,
+      DtlsSrtpKeyAgreement: true,
     },
   ],
   mandatory: {
@@ -40,7 +72,7 @@ alice.onnegotiationneeded = function () {
         bob.createAnswer(function (sdp) {
           bob.setLocalDescription(sdp, function () {
             alice.setRemoteDescription(sdp, function () {
-
+              console.log('Alice -> Bob Connected!');
             });
           });
         });
@@ -56,7 +88,7 @@ bob.onnegotiationneeded = function () {
         alice.createAnswer(function (sdp) {
           alice.setLocalDescription(sdp, function () {
             bob.setRemoteDescription(sdp, function () {
-
+              console.log('Bob -> Alice Connected!');
             });
           });
         });
@@ -65,14 +97,33 @@ bob.onnegotiationneeded = function () {
   });
 };
 
+alice.onaddstream = function(stream) {
+  console.log('Alice: mediaStream added!');
+};
+
+bob.onaddstream = function(stream) {
+  console.log('Bob: mediaStream added!');
+};
+
+alice.onremovestream = function(stream) {
+  console.log('Alice: mediaStream removed!');
+};
+
+bob.onremovestream = function(stream) {
+  console.log('Bob: mediaStream removed!');
+};
+
 WEBRTC.getSources(function (inputs) {
   console.log(inputs);
 
-  WEBRTC.getUserMedia({
-    audio: true,
-    video: true,
-  }, function (stream) {
-    alice.addStream(stream);
+  WEBRTC.getUserMedia(constraints, function(stream) {
+    if (stream) {
+      alice.addStream(stream);
+
+      setTimeout(function () {
+        alice.close();
+        bob.close();
+      }, 5000);
+    }
   });
 });
-
