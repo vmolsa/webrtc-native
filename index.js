89582def6fa20d59a77492d0bb7a6909c2ae1c17
Ville Mölsä <ville.molsa@gmail.com>
2015-06-28 23:26:36 +0300

    Update index

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-try {
-  module.exports = require('./build/Release/webrtc-native.node');
-} catch (ignored) {
-  module.exports = require('./build/Debug/webrtc-native.node');
-}
+
+module.exports = require('./build/Release/webrtc.node');
