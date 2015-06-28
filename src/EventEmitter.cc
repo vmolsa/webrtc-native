656a645f8a744d93fc2ff7c615a355b8d1044316
vmolsa <ville.molsa@gmail.com>
2015-06-28 08:38:39 +0300

    Update EventEmitter

--- src/EventEmitter.cc
+++ src/EventEmitter.cc
@@ -27,26 +27,81 @@
 
 using namespace WebRTC;
 
-EventEmitter::EventEmitter() :
-  _running(false),
-  _closing(false)
-{
+EventEmitter::EventEmitter() {
   LOG(LS_INFO) << __PRETTY_FUNCTION__;
   
   uv_mutex_init(&_lock);
   _async.data = this;
+  
+  uv_async_init(uv_default_loop(), &_async, reinterpret_cast<uv_async_cb>(EventEmitter::onAsync));
+  
+  EventEmitter::SetReference(false);
 }
 
 EventEmitter::~EventEmitter() {
   LOG(LS_INFO) << __PRETTY_FUNCTION__;
-  
-  EventEmitter::End();
 
+  uv_close(reinterpret_cast<uv_handle_t*>(&_async), NULL);
+    
   while (!_events.empty()) {
     Event *event = _events.front();
     _events.pop();
     delete event;
   }
 
-  uv_mutex_destroy(&_lock); 
+  uv_mutex_destroy(&_lock);
 }
+
+void EventEmitter::SetReference(bool alive) {
+  uv_mutex_lock(&_lock);
+  
+  if (alive) {
+    uv_ref(reinterpret_cast<uv_handle_t*>(&_async));
+  } else {
+    uv_unref(reinterpret_cast<uv_handle_t*>(&_async));
+  }
+  
+  uv_mutex_unlock(&_lock);
+}
+
+void EventEmitter::Emit(int event) {
+  LOG(LS_INFO) << __PRETTY_FUNCTION__;
+  EventEmitter::Emit(new Event(event));
+}
+
+void EventEmitter::Emit(Event *event) {
+  LOG(LS_INFO) << __PRETTY_FUNCTION__;
+  
+  uv_mutex_lock(&_lock);
+  
+  _events.push(event);  
+  uv_async_send(&_async);
+  
+  uv_mutex_unlock(&_lock);
+}
+
+void EventEmitter::onAsync(uv_async_t *handle, int status) {
+  LOG(LS_INFO) << __PRETTY_FUNCTION__;
+  
+  EventEmitter *self = static_cast<EventEmitter*>(handle->data);
+  self->DispatchEvents();
+ }
+ 
+ void EventEmitter::DispatchEvents() {
+  LOG(LS_INFO) << __PRETTY_FUNCTION__;
+  uv_mutex_lock(&_lock);
+
+  while (!_events.empty()) {
+    Event *event = _events.front();
+    _events.pop();
+    
+    uv_mutex_unlock(&_lock);
+    
+    On(event);
+    delete event;
+    
+    uv_mutex_lock(&_lock);
+  }
+  
+  uv_mutex_unlock(&_lock);
+ }
