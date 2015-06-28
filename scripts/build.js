45be703dec82e8e61878d210154e820f1a23fd14
Ville Mölsä <ville.molsa@gmail.com>
2015-06-28 20:03:38 +0300

    Remove execSync

--- scripts/build.js
+++ scripts/build.js
@@ -1,13 +1,8 @@
 var fs = require('fs');
 var os = require('os');
-var sh = require('child_process').execSync;
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
 var path = require('path');
-
-if (!sh) {
-  sh = require('sync-exec');
-}
 
 var ROOT = process.cwd();
 var NODEJS = '.';
@@ -56,22 +51,42 @@ if (os.platform() == 'win32' && process.arch == 'x64') {
   WEBRTC_OUT = path.resolve(WEBRTC_SRC, 'out', CONFIG + '_x64');
 }
 
+function install() {
+  fs.linkSync(WEBRTC_OUT + path.sep + 'webrtc-native.node', ROOT + path.sep + 'build' + path.sep + CONFIG + path.sep + 'webrtc-native.node');
+  console.log('Done! :)');
+}
+
+function compile() {
+  var res = spawn('ninja', [ '-C', WEBRTC_OUT ], {
+    cwd: WEBRTC_SRC,
+    env: process.env,
+    stdio: 'inherit',
+  });
+
+  res.on('close', function (code) {
+    if (!code) {
+      return install();
+    }
+
+    process.exit(1);
+  }); 
+}
+
 function build() {
   if (fs.existsSync(WEBRTC_SRC)) {
-    sh('python ' + WEBRTC_SRC + path.sep + 'webrtc' + path.sep + 'build' + path.sep + 'gyp_webrtc src' + path.sep + 'webrtc.gyp', {
+    var res = spawn('python', [ WEBRTC_SRC + path.sep + 'webrtc' + path.sep + 'build' + path.sep + 'gyp_webrtc', 'src' + path.sep + 'webrtc.gyp' ], {
       cwd: ROOT,
       env: process.env,
       stdio: 'inherit',
     });
-
-    sh('ninja -C ' + WEBRTC_OUT, {
-      cwd: WEBRTC_SRC,
-      env: process.env,
-      stdio: 'inherit',
+  
+    res.on('close', function (code) {
+      if (!code) {
+        return compile();
+      }
+  
+      process.exit(1);
     });
-
-    fs.linkSync(WEBRTC_OUT + path.sep + 'webrtc-native.node', ROOT + path.sep + 'build' + path.sep + CONFIG + path.sep + 'webrtc-native.node');
-    console.log('Done! :)');
   }
 }
 
