d19e7f13d7319e1cdb447604cb6035e039b33147
Ville Mölsä <ville.molsa@gmail.com>
2015-07-26 22:26:34 +0300

    Update test

--- test/test.js
+++ test/test.js
@@ -1,13 +1,20 @@
 var WebRTC = require('../');
 
-//WebRTC.setDebug(true);
+WebRTC.setDebug(true);
 
-var capturer = new WebRTC.MediaSource('webcam');
 var renderer = new WebRTC.MediaSource('window');
+var capturer = new WebRTC.MediaSource('webcam');
+
+capturer.onend = function() {
+  renderer.end();
+};
+
+capturer.ondata = function(data) {
+  renderer.write(data);
+};
 
 setTimeout(function() {
   console.log('Closing...');
   
   capturer.end();
-  renderer.end();
 }, 5000);
