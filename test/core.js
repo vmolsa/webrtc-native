e030e949483a2c78f4594b4da6742d14a2c2faf7
Ville Mölsä <villemolsa@Ville-MacBook-Pro.local>
2016-03-12 08:34:18 -0800

    Update core

--- test/core.js
+++ test/core.js
@@ -1,3 +1,7 @@
 var WebRTC = require('../');
 
-console.log('WebRTC Core Loaded! Closing now...');
+console.log('WebRTC Core Loaded!');
+
+setTimeout(function() {
+  console.log('Closing now...');
+}, 1000);
