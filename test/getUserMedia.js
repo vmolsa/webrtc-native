163552c0a6602c6e27039c077677df9a4840b6c4
Ville Mölsä <villemolsa@Ville-MacBook-Pro.local>
2016-03-12 16:48:12 -0800

    Update getUserMedia

--- test/getUserMedia.js
+++ test/getUserMedia.js
@@ -2,8 +2,18 @@ var WebRTC = require('../');
 
 //WebRTC.setDebug(true);
 
+function dumpTrack(track) {
+  console.log('MediaStreamTrack Enabled:', track.enabled);
+  console.log('MediaStreamTrack Id:', track.id);
+  console.log('MediaStreamTrack Kind:', track.kind);
+  console.log('MediaStreamTrack Label:', track.label);
+  console.log('MediaStreamTrack Muted:', track.muted);
+  console.log('MediaStreamTrack ReadyState:', track.readyState);
+  console.log('MediaStreamTrack Remote:', track.remote);
+}
+
 function onSuccess(stream) {
-  if (stream) {
+  if (stream && stream.active) {
     stream.onaddtrack = function(track) {
       console.log('Track Added!');
     };
@@ -17,14 +27,17 @@ function onSuccess(stream) {
     console.log('MediaStream ID:', stream.id);
 
     var audio_list = stream.getAudioTracks();
+    
     audio_list.forEach(function (track) {
       console.log('Audio Track');
+      dumpTrack(track);
     });
 
     var video_list = stream.getVideoTracks();
     
     video_list.forEach(function (track) {
       console.log('Video Track');
+      dumpTrack(track);
     });
     
     setTimeout(function() {
