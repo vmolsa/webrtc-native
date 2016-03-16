a08b7b002fa759aaccdbfab29efc3abef5496b0f
Ville Mölsä <villemolsa@Ville-MacBook-Pro.local>
2016-03-16 20:07:40 +0200

    Switch to branch 50

--- scripts/build.js
+++ scripts/build.js
@@ -10,7 +10,7 @@ if (!fs.existsSync(ROOT + path.sep + 'build' + path.sep + 'config.gypi')) {
   throw new Error('Run node-gyp rebuild instead of node build.js');
 }
 
-var CHROMIUM_BRANCH = null; //'branch-heads/47';
+var CHROMIUM_BRANCH = 'branch-heads/50';
 var USE_OPENSSL = false;
 var USE_GTK = false;
 var USE_X11 = false;
