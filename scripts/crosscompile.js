486d6e2c06fb867c0d0258f694a12ea3b09df20c
vmolsa <ville.molsa@gmail.com>
2017-05-09 12:21:30 +0300

    Build in verbose mode

--- scripts/crosscompile.js
+++ scripts/crosscompile.js
@@ -6,7 +6,7 @@ const root_path = path.resolve(__dirname, '../');
 
 function runCross() {
   console.log('Cross compiling:', process.env['npm_config_arch']);
-  const ng = spawn('node', ['node_modules/node-gyp/bin/node-gyp.js', 'rebuild', '--release', '--arch=' + process.env['npm_config_arch']]);
+  const ng = spawn('node', ['node_modules/node-gyp/bin/node-gyp.js', 'rebuild', '-v', '--release', '--arch=' + process.env['npm_config_arch']]);
 
   ng.stdout.pipe(process.stdout);
   ng.stderr.pipe(process.stderr);
